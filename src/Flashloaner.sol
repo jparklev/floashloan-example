5abaffd1b3e3f49087cb3560c49c9de6371edcdf
Josh Levine <joshuapark95@gmail.com>
2022-03-30 11:44:48 -0400

    feat: init basic flash loan ex

---
+++ src/Flashloaner.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+
+interface FlashloanCallback {
+    function receiveTokens(address tokenAddress, uint256 amountToBorrow) external;
+}
+
+contract Flashloaner {
+    ERC20 public token;
+    uint256 public balance;
+
+    constructor(address _token) {
+        token = ERC20(_token);
+    }
+
+    function deposit(uint256 _amount) public {
+        token.transferFrom(msg.sender, address(this), _amount);
+        balance += _amount;
+    }
+
+    function withdraw(uint256 _amount) public {
+        token.transfer(msg.sender, _amount);
+        balance -= _amount;
+    }
+
+    function flashloan(uint256 amountToBorrow) public {
+        require(amountToBorrow <= balance, "NOT_ENOUGH_TOKENS");
+        require(balance == token.balanceOf(address(this)), "UNEXPECTED_BALANCE");
+
+        token.transfer(msg.sender, amountToBorrow);
+
+        FlashloanCallback(msg.sender).receiveTokens(address(token), amountToBorrow);
+
+        require(balance <= token.balanceOf(address(this)), "TOO_FEW_TOKENS_RETURNED");
+    }
+}
---
+++ src/test/Flashloaner.t.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "ds-test/test.sol";
+
+import { Flashloaner } from "../Flashloaner.sol"; 
+import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
+
+contract FlashloanerTest is DSTest {
+    uint256 public constant INITIAL_BALANCE = 1e18;
+
+    Flashloaner public flashloaner;
+    MockERC20 public token;
+
+    // Balance that will be returned to the flashloaner when the `receiveTokens` callback is called
+    uint256 public _returnAmount;
+
+    function setUp() public {
+        token = new MockERC20("APE TOKEN", "APE", 18);
+
+        // Mint initial Ape tokens to this address
+        token.mint(address(this), INITIAL_BALANCE);
+
+        flashloaner = new Flashloaner(address(token));
+    }
+
+    /* ========== FLASHLOAN CALLBACK ========== */
+    /// This function will be called back into when this contract calls `flashloan` on the Flashloaner
+    function receiveTokens(address tokenAddress, uint256 amountToBorrow) public {
+        // Do sick arbs with the funds
+        token.transfer(address(msg.sender), _returnAmount);
+    }
+    /* ======================================== */
+
+    function testDeposit() public {
+        token.approve(address(flashloaner), INITIAL_BALANCE);
+        flashloaner.deposit(INITIAL_BALANCE);
+
+        assertEq(token.balanceOf(address(flashloaner)), INITIAL_BALANCE);
+        assertEq(token.balanceOf(address(flashloaner)), flashloaner.balance());
+    }
+
+    function testFuzzWithdraw(uint256 amount) public {
+        // Only test fuzz params that are less than the balance of APE tokens in this contract
+        if (amount > INITIAL_BALANCE) {
+            return;
+        }
+        
+        token.approve(address(flashloaner), amount);
+        flashloaner.deposit(amount);
+        // Tokens leave this contract when deposited
+        assertEq(token.balanceOf(address(this)), INITIAL_BALANCE - flashloaner.balance());
+
+        flashloaner.withdraw(amount);
+        // No funds are left behind in the flashloaner
+        assertEq(token.balanceOf(address(this)), INITIAL_BALANCE);
+    }
+
+    function testBasicFlashLoan() public {
+        // Give the flashloaner half of the APE tokens in this contract
+        token.approve(address(flashloaner), INITIAL_BALANCE / 2);
+        flashloaner.deposit(INITIAL_BALANCE  / 2);
+
+        // Borrow and return the full balance available
+        uint256 flashloanerBalancePre = flashloaner.balance();
+        _returnAmount = flashloanerBalancePre;
+        // Should not revert
+        flashloaner.flashloan(flashloanerBalancePre);
+
+        assertEq(token.balanceOf(address(this)), INITIAL_BALANCE - flashloanerBalancePre);
+        assertEq(flashloanerBalancePre, flashloaner.balance());
+    }
+
+    function testFlashFailingCondition() public {
+        // TODO
+
+        // HINT: this is an example for how to assert a failure
+        // try flashloaner.flashloan(amountToBorrow) {
+        //     fail();
+        // } catch (string memory error) {
+        //     assertEq(error, "NOT_ENOUGH_TOKENS");
+        // }
+    }
+
+    // TOOD: add more tests
+}
