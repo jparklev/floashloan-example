da05565dae751ce9eba6066a804afccfc18048ea
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-04-07 19:36:34 +0100

    chore: fix and add tests

--- src/Flashloaner.sol
+++ src/Flashloaner.sol
@@ -9,30 +9,35 @@ interface FlashloanCallback {
 
 contract Flashloaner {
     ERC20 public token;
-    uint256 public balance;
+    address public owner;
 
     constructor(address _token) {
         token = ERC20(_token);
+        owner = msg.sender;
     }
 
     function deposit(uint256 _amount) public {
         token.transferFrom(msg.sender, address(this), _amount);
-        balance += _amount;
     }
 
-    function withdraw(uint256 _amount) public {
+    // only owner can withdraw (or we would have to allow multiple people being able to deposit/withdraw and handle balances separately)
+    function withdraw(uint256 _amount) requiresAuth public nonReentrant {
         token.transfer(msg.sender, _amount);
-        balance -= _amount;
     }
 
     function flashloan(uint256 amountToBorrow) public {
-        require(amountToBorrow <= balance, "NOT_ENOUGH_TOKENS");
-        require(balance == token.balanceOf(address(this)), "UNEXPECTED_BALANCE");
+        require(amountToBorrow <= balancePreLoan, "NOT_ENOUGH_TOKENS");
+        uint256 balancePreLoan = token.balanceOf(address(this));
 
         token.transfer(msg.sender, amountToBorrow);
 
         FlashloanCallback(msg.sender).receiveTokens(address(token), amountToBorrow);
 
-        require(balance <= token.balanceOf(address(this)), "TOO_FEW_TOKENS_RETURNED");
+        require(balancePreLoan == token.balanceOf(address(this)), "TOO_FEW_TOKENS_RETURNED");
+    }
+
+    modifier requiresAuth() {
+        require(owner == msg.sender, "UNAUTHORIZED");
+        _;
     }
 }
--- src/test/Flashloaner.t.sol
+++ src/test/Flashloaner.t.sol
@@ -6,6 +6,19 @@ import "ds-test/test.sol";
 import { Flashloaner } from "../Flashloaner.sol"; 
 import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
 
+
+contract User { 
+    Flashloaner public flashloaner;
+
+    constructor(address _flashloaner) {
+        flashloaner = Flashloaner(_flashloaner);
+    }
+
+    function doWithdraw(uint256 amount) public {
+        flashloaner.withdraw(amount);
+    }
+}
+
 contract FlashloanerTest is DSTest {
     uint256 public constant INITIAL_BALANCE = 1e18;
 
@@ -17,10 +30,6 @@ contract FlashloanerTest is DSTest {
 
     function setUp() public {
         token = new MockERC20("APE TOKEN", "APE", 18);
-
-        // Mint initial Ape tokens to this address
-        token.mint(address(this), INITIAL_BALANCE);
-
         flashloaner = new Flashloaner(address(token));
     }
 
@@ -33,54 +42,130 @@ contract FlashloanerTest is DSTest {
     /* ======================================== */
 
     function testDeposit() public {
+        // Mint initial Ape tokens to this address
+        token.mint(address(this), INITIAL_BALANCE);
         token.approve(address(flashloaner), INITIAL_BALANCE);
+
+        uint256 balBefore = token.balanceOf(address(flashloaner));
         flashloaner.deposit(INITIAL_BALANCE);
 
         assertEq(token.balanceOf(address(flashloaner)), INITIAL_BALANCE);
-        assertEq(token.balanceOf(address(flashloaner)), flashloaner.balance());
+        assertEq(token.balanceOf(address(flashloaner)), balBefore + INITIAL_BALANCE);
     }
 
-    function testFuzzWithdraw(uint256 amount) public {
-        // Only test fuzz params that are less than the balance of APE tokens in this contract
-        if (amount > INITIAL_BALANCE) {
-            return;
-        }
-        
-        token.approve(address(flashloaner), amount);
-        flashloaner.deposit(amount);
-        // Tokens leave this contract when deposited
-        assertEq(token.balanceOf(address(this)), INITIAL_BALANCE - flashloaner.balance());
+    function testWithdraw() public {
+        token.mint(address(this), INITIAL_BALANCE);
+        token.approve(address(flashloaner), INITIAL_BALANCE);
+        flashloaner.deposit(INITIAL_BALANCE);
 
-        flashloaner.withdraw(amount);
+        // Tokens leave this contract when deposited
+        assertEq(token.balanceOf(address(this)), INITIAL_BALANCE - token.balanceOf(address(flashloaner)));
+
+        flashloaner.withdraw(INITIAL_BALANCE);
+
         // No funds are left behind in the flashloaner
         assertEq(token.balanceOf(address(this)), INITIAL_BALANCE);
     }
 
+    function testWithdrawIfNotOwner() public {
+        User user = new User(address(flashloaner));
+
+        token.mint(address(this), INITIAL_BALANCE);
+        token.approve(address(flashloaner), INITIAL_BALANCE);
+        flashloaner.deposit(INITIAL_BALANCE);
+
+        try user.doWithdraw(INITIAL_BALANCE) {
+            fail();
+        } catch Error(string memory error) {
+            assertEq(error, "UNAUTHORIZED");
+        }
+    }
+
     function testBasicFlashLoan() public {
+        token.mint(address(this), INITIAL_BALANCE);
+
         // Give the flashloaner half of the APE tokens in this contract
         token.approve(address(flashloaner), INITIAL_BALANCE / 2);
         flashloaner.deposit(INITIAL_BALANCE  / 2);
 
         // Borrow and return the full balance available
-        uint256 flashloanerBalancePre = flashloaner.balance();
+        uint256 flashloanerBalancePre = token.balanceOf(address(flashloaner));
         _returnAmount = flashloanerBalancePre;
         // Should not revert
         flashloaner.flashloan(flashloanerBalancePre);
 
         assertEq(token.balanceOf(address(this)), INITIAL_BALANCE - flashloanerBalancePre);
-        assertEq(flashloanerBalancePre, flashloaner.balance());
+        assertEq(flashloanerBalancePre, token.balanceOf(address(flashloaner)));
     }
 
-    function testFlashFailingCondition() public {
-        // TODO
-
-        // HINT: this is an example for how to assert a failure
-        // try flashloaner.flashloan(100e18) {
-        //     fail();
-        // } catch Error(string memory error) {
-        //     assertEq(error, "NOT_ENOUGH_TOKENS");
-        // }
+    function testCantBorrowMoreThanBalance() public {
+        try flashloaner.flashloan(100e18) {
+            fail();
+        } catch Error(string memory error) {
+            assertEq(error, "NOT_ENOUGH_TOKENS");
+        }
     }
 
-    // TOOD: add more tests
+    // fuzz tests
+    function testFuzzDeposit(uint256 amount) public {
+        token.mint(address(this), amount);
+        token.approve(address(flashloaner), amount);
+
+        uint256 balBefore = token.balanceOf(address(flashloaner));
+        flashloaner.deposit(amount);
+
+        assertEq(token.balanceOf(address(flashloaner)), amount);
+        assertEq(token.balanceOf(address(flashloaner)), balBefore + amount);
+    }
+
+    function testFuzzWithdraw(uint256 amount) public {
+        token.mint(address(this), amount);
+        token.approve(address(flashloaner), amount);
+        flashloaner.deposit(amount);
+
+        // Tokens leave this contract when deposited
+        assertEq(token.balanceOf(address(this)), amount - token.balanceOf(address(flashloaner)));
+
+        flashloaner.withdraw(amount);
+
+        // No funds are left behind in the flashloaner
+        assertEq(token.balanceOf(address(this)), amount);
+    }
+
+    function testFuzzFlashLoan(uint256 amount) public {
+        token.mint(address(this), amount);
+        token.approve(address(flashloaner), amount);
+        flashloaner.deposit(amount);
+
+        uint256 userBalPreLoan = token.balanceOf(address(this));
+        uint256 flashloanerBalPreLoan = token.balanceOf(address(flashloaner));
+        _returnAmount = amount;
+        flashloaner.flashloan(_returnAmount);
+
+        assertEq(userBalPreLoan, token.balanceOf(address(this)));
+        assertEq(flashloanerBalPreLoan, token.balanceOf(address(flashloaner)));
+    }
+
+    function testFuzzFlashloanWhenAirdrop(uint256 amount) public {
+        token.mint(address(this), amount);
+        token.transfer(address(flashloaner), amount);
+
+        _returnAmount = amount / 2;
+        flashloaner.flashloan(_returnAmount);
+    }
+
+    function testFuzzCantReturnLessThanBorrowed(uint256 amount) public {
+        if (amount == 0) amount = 1;
+        token.mint(address(this), amount);
+        token.approve(address(flashloaner), amount);
+        flashloaner.deposit(amount);
+
+        _returnAmount = amount - 1;
+        try flashloaner.flashloan(amount) {
+            fail();
+        } catch Error(string memory error) {
+            assertEq(error, "TOO_FEW_TOKENS_RETURNED");
+        }
+    }
+
 }
